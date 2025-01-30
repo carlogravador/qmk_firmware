0e2aa54df37a304189fedb1192eda8f05b0f2a64
Carlo Gravador <cce.gravador@gmail.com>
2025-01-30 18:59:02 +0100

    Update keymaps

--- keyboards/splitkb/aurora/lily58/keymaps/carlogravador/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/carlogravador/keymap.c
@@ -103,16 +103,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC , KC_ENT , MO(1)  , KC_MINS, KC_EQL
     ),
     [_RAISE] = LAYOUT(
-        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                   _______, _______, _______, KC_MINS, KC_EQL , _______,
-        KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                   _______, _______, _______, KC_LBRC, KC_RBRC, _______,
-        KC_CAPS, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VPTB, KC_VNTB, _______, _______,
-                                   _______, _______, MO(2)  , _______, _______, _______, _______, _______
+        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL , KC_DEL ,
+        KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                   XXXXXXX, KC_HOME, KC_END , KC_LBRC, KC_RBRC, XXXXXXX,
+        KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX ,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TVMO, KC_TPTB, KC_TNTB, KC_VPTB, KC_VNTB, XXXXXXX, MO(2)  ,
+                                   _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RGB_EFFECT] = LAYOUT(
-        RM_ANWT, RM_EFF1, RM_EFF2, RM_EFF3, RM_EFF4, RM_EFF5,                   RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   RM_NEXT, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, XXXXXXX,
-        KC_TVMO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   RM_PREV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RM_ANWT, RM_EFF1, RM_EFF2, RM_EFF3, RM_EFF4, RM_EFF5,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RM_TOGG, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TPTB, KC_TNTB, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
